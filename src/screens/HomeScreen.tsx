e1c4937088d4f835f05815b636c406caa909bd12
RyanOC20 <ryoc20@gmail.com>
2025-11-19 23:16:16 -0800

    Added weekly status report for 11/10

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -6,7 +6,7 @@ export function HomeScreen(): TSX.Element {
   return (
     <View style={styles.container}>
       <Image
-        source={require('../../assets/logo.png')}
+        source={require('../assets/logo.png')}
         style={styles.logo}
         resizeMode="contain"
       />
